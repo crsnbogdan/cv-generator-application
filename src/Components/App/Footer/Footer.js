11b28edd22e1d0817bbbc0a153448967aac0dd85
crsnbogdan <crsnbogdan@gmail.com>
2022-08-31 17:52:45 +0300

    turn Footer from a class component into a functional component

--- src/Components/App/Footer/Footer.js
+++ src/Components/App/Footer/Footer.js
@@ -1,3 +1,30 @@
+import React from "react";
+
+const Footer = (props) => {
+  return (
+    <footer className="flex align-center justify-center p-10">
+      <div className="inline-flex flex-col">
+        {props.mode === "previewing" ? (
+          <button
+            id="footer-downloadbtn"
+            className=" bg-blue-400 text-white px-6 py-3 text-lg rounded-lg mb-6"
+            onClick={props.generatePDFFunc}
+          >
+            Print CV
+          </button>
+        ) : null}
+        <p className="text-lg">
+          <span className="text-blue-400">© {new Date().getFullYear()}</span> -
+          Crisan Bogdan
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
+/*
 import React, { Component } from "react";
 class Footer extends Component {
   constructor(props) {
@@ -28,3 +55,4 @@ class Footer extends Component {
 }
 
 export default Footer;
+*/
