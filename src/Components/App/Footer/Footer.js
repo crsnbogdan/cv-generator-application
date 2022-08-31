e6b9f28997fe90c917f1b181fb81c5cd73baf8ba
crsnbogdan <crsnbogdan@gmail.com>
2022-08-31 18:17:42 +0300

    turn Nav from a class component into a function component and update CSS classes to fit the BEM naming convention

--- src/Components/App/Footer/Footer.js
+++ src/Components/App/Footer/Footer.js
@@ -23,36 +23,3 @@ const Footer = (props) => {
 };
 
 export default Footer;
-
-/*
-import React, { Component } from "react";
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-  }
-  state = {};
-  render() {
-    return (
-      <footer className="footer flex align-center justify-center p-10">
-        <div className="inline-flex flex-col">
-          {this.props.mode === "previewing" ? (
-            <button
-              id="footer-downloadbtn"
-              className=" bg-blue-400 text-white px-6 py-3 text-lg rounded-lg mb-6"
-              onClick={this.props.generatePDFFunc}
-            >
-              Print CV
-            </button>
-          ) : null}
-          <p className="footer--text text-lg">
-            <span className="text-blue-400">© {new Date().getFullYear()}</span>{" "}
-            - Crisan Bogdan
-          </p>
-        </div>
-      </footer>
-    );
-  }
-}
-
-export default Footer;
-*/
--- src/Components/App/Nav/Nav.js
+++ src/Components/App/Nav/Nav.js
@@ -1,41 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Nav.css";
-class Nav extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div
-        id="nav"
-        className="nav z-40 flex justify-between items-center px-20 bg-white"
-      >
-        {this.props.mode === "editing" ? (
-          <div className="navtext">
-            <h1 className="font-bold text-3xl text-blue-400">CV Generator</h1>
-            <p className="my-1 text-lg text-gray-400">Currently editing</p>
-          </div>
-        ) : (
-          <div className="navtext">
-            <h1 className="font-bold text-3xl text-gray-400">CV Generator</h1>
-            <p className="my-1 text-lg text-blue-400">Currently previewing</p>
-          </div>
-        )}
 
-        {this.props.mode === "previewing" ? (
-          <a href="#footer-downloadbtn" className="text-blue-400">
-            Go to print
-          </a>
-        ) : null}
-        <div className="modetoggle">
-          <input type="checkbox" id="switch" onChange={this.props.switchMode} />
-          <label htmlFor="switch" className="switchLabel">
-            Toggle
-          </label>
+const Nav = (props) => {
+  return (
+    <nav className="nav z-40 flex justify-between items-center px-20 bg-white">
+      {props.mode === "editing" ? (
+        <div className="nav__text">
+          <h1 className="font-bold text-3xl text-blue-400">CV Generator</h1>
+          <p className="my-1 text-lg text-gray-400">Currently editing</p>
         </div>
+      ) : (
+        <div className="nav__text">
+          <h1 className="font-bold text-3xl text-gray-400">CV Generator</h1>
+          <p className="my-1 text-lg text-blue-400">Currently previewing</p>
+        </div>
+      )}
+      {props.mode === "previewing" ? (
+        <a href="#footer___downloadbtn" className="text-blue-400">
+          Go to print
+        </a>
+      ) : null}
+      <div className="nav__modetoggle">
+        <input type="checkbox" id="switch" onChange={props.switchMode} />
+        <label htmlFor="switch" className="nav__modetoggle--switchlabel">
+          Toggle
+        </label>
       </div>
-    );
-  }
-}
+    </nav>
+  );
+};
 
 export default Nav;
